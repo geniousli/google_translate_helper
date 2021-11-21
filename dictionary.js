0e0d3c2b31401e7b3f794e815178898cf411487e
wildimagine <wildimagine@qq.com>
2021-11-21 10:12:30 +0800

    [fix]: refactor & add link

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,7 @@
+// const iqwerty = require('./toast.js');
 window.addEventListener("click", clickEvent);
 window.localStorage.__proto__.getItemSplit = function(name, split, def = []) {
-  let val = this.getItem(name);
+  var val = this.getItem(name);
   if(val) {
     return val.split(split);
   }else {
@@ -8,25 +9,25 @@ window.localStorage.__proto__.getItemSplit = function(name, split, def = []) {
   }
 }
 
-// let ALL_CONTENTKEY = "all_content"; // content key
-let DATEKEY = "date_key"; // save all keys of key
+// var ALL_CONTENTKEY = "all_content"; // content key
+var DATEKEY = "date_key"; // save all keys of key
 
 // date key should like this
 function todayKey() {
-  let time = new Date();
-  let today = time.toISOString().substr(0, 10);
+  var time = new Date();
+  var today = time.toISOString().substr(0, 10);
   return `${today}-contents`;
 }
 
 function addSaveBtn() {
-  let dom = document.getElementsByClassName('AxqVh')[0];
-  let info = '<div class="saveBtnDiv"><p class="saveBtnP" sign="saveBtn"> save words </p><div class="saveBtnPadding"></div></div>';
-  let doc = new DOMParser().parseFromString(info, "text/xml");
+  var dom = document.getElementsByClassName('AxqVh')[0];
+  var info = '<div class="saveBtnDiv"><p class="saveBtnP" sign="saveBtn"> save words </p><div class="saveBtnPadding"></div></div>';
+  var doc = new DOMParser().parseFromString(info, "text/xml");
   dom.appendChild(doc.firstChild);
 }
 
 const domChangeCallback = function(mutationsList, observer) {
-  let targetExist = false;
+  var targetExist = false;
   for(const mutation of mutationsList) {
     if (mutation.type === 'childList' && mutation.target && mutation.target.className == "aia4Ud") {
       targetExist = true;
@@ -41,73 +42,101 @@ const domChangeCallback = function(mutationsList, observer) {
 };
 
 function addDicBtn() {
-  let dom = document.getElementsByClassName('aia4Ud')[0];
+  var dom = document.getElementsByClassName('aia4Ud')[0];
   if(!dom.getElementsByClassName('saveDicBtnDiv')[0]) {
-    let info = '<div class="saveDicBtnDiv"><span >add to dic</span></div>';
-    let doc = new DOMParser().parseFromString(info, "text/xml");
+    var info = '<div class="saveDicBtnDiv" ><span sign="saveDicBtnDiv">add to dic</span></div>';
+    var doc = new DOMParser().parseFromString(info, "text/xml");
     dom.appendChild(doc.firstChild);
   }
 }
 
 function observeDomChange() {
-  let dom = document.getElementsByClassName('kGmWO')[0];
-  let cwiz = dom.getElementsByTagName('c-wiz')[0];
+  var dom = document.getElementsByClassName('kGmWO')[0];
+  var cwiz = dom.getElementsByTagName('c-wiz')[0];
   const config = { childList: true, subtree: true };
   const observer = new MutationObserver(domChangeCallback);
   observer.observe(cwiz, config);
 }
 
-function initHistoryDiv() {
-  let fankui = document.getElementsByClassName('a88hkc')[0];
+const historyFromStorage = (function() {
+  var map = {}; // data => []
+  var dates = []; // [2021-11-01, '2021-12-01']
+  var allContents = []; // ['xx', 'xx1]
 
-  let allContents = '';
-  let allDates = window.localStorage.getItemSplit(DATEKEY, ',', []);
-  allDates.forEach(function(item){
-    let contents = window.localStorage.getItem(item);
-    allContents += contents;
-  });
-  let div = historyDiv(allContents, 'history');
-  let doc = new DOMParser().parseFromString(div, "text/xml");
-  let parent = document.getElementsByClassName('WFnNle')[0].firstChild;
-  parent.insertBefore(doc.firstChild, fankui);
+  var fankui = document.getElementsByClassName('a88hkc')[0];
+  var parent = document.getElementsByClassName('WFnNle')[0].firstChild;
 
+  function toDiv(contents, title) {
+    var cSpan = '';
+    contents.forEach(function(item) {
+      if(item) {
+        cSpan += toSpan(item);
+      }
+    });
+    var history = `<div class="historyDiv"><p class="historyTitle">${title}:</p><span class="historyContainer" > ${cSpan}</span></div>`;
+    return history;
+  }
 
-  allDates.slice(0, 4).forEach(function(item){
-    let contents = window.localStorage.getItem(item);
-    let div = historyDiv(contents, item.substr(0, 10));
-    let doc = new DOMParser().parseFromString(div, "text/xml");
+  function toSpan(content) {
+    return '<span class="historySpan">' + content + '</span>';
+  }
+
+  function insertLastFiveDay() {
+    var div = toDiv(allContents, 'history');
+    var doc = new DOMParser().parseFromString(div, "text/xml");
     parent.insertBefore(doc.firstChild, fankui);
-  });
 
-}
+    dates.slice(0, 4).forEach(function(item){
+      var contents = map[item];
+      var div = toDiv(contents, item.substr(0, 10));
+      var doc = new DOMParser().parseFromString(div, "text/xml");
+      parent.insertBefore(doc.firstChild, fankui);
+    });
+  }
 
-function historyDiv(contents, title) {
-  let cSpan = '';
-  contents.split('\n').forEach(function(item) {
-    cSpan += historySpan(item);
-  });
-  let history = `<div class="historyDiv"><p class="historyTitle">${title}:</p><span class="historyContainer"> ${cSpan}</span></div>`;
-  return history;
-}
+  function insertHistoryLink() {
+    let div = `<div class="historyDiv"><a class="historyLink" href="#">all histories</a></div>`;
+    var doc = new DOMParser().parseFromString(div, "text/xml");
+    parent.insertBefore(doc.firstChild, fankui);
+  }
 
-function historySpan(content) {
-  return '<span class="historySpan">' + content + '</span>';
+  function loadData() {
+    dates = window.localStorage.getItemSplit(DATEKEY, ',', []);
+    dates.reverse();
+    dates.forEach(function(item){
+      var contents = window.localStorage.getItemSplit(item, '\n', []);
+      map[item] = contents;
+      allContents = allContents.concat(contents);
+    });
+  }
+
+  return {
+    loadData: loadData,
+    insertLastFiveDay: insertLastFiveDay,
+    insertHistoryLink: insertHistoryLink,
+  };
+})();
+
+
+function initHistoryDiv() {
+  historyFromStorage.loadData();
+  historyFromStorage.insertLastFiveDay();
+  historyFromStorage.insertHistoryLink();
 }
 
 // 1. save to today key , 2. save to all content, 3. add date key to datekey
-function saveWord() {
-  let newContent = getTranslateContent();
+function saveWord(newContent) {
   if(newContent) {
-    let key = todayKey();
-    let todayCons = window.localStorage.getItemSplit(key, '\n', []);
-    for (let word of newContent.split('\n')) {
+    var key = todayKey();
+    var todayCons = window.localStorage.getItemSplit(key, '\n', []);
+    for (var word of newContent.split('\n')) {
       if(word && todayCons.indexOf(word) <= -1) {
         todayCons.push(word);
       }
     }
     window.localStorage.setItem(key, todayCons.join('\n'));
 
-    let ary = window.localStorage.getItemSplit(DATEKEY, ',', []);
+    var ary = window.localStorage.getItemSplit(DATEKEY, ',', []);
     if(ary.indexOf(key) <= -1) {
       ary.push(key);
     }
@@ -115,23 +144,59 @@ function saveWord() {
   }
 }
 
+function getSpecialWordInfo() {
+  var dom = document.getElementsByClassName('aia4Ud')[0];
+  if(dom){
+    return dom.firstChild.textContent;
+  }
+}
+
 function clickEvent(event) {
   if(event.target.getAttribute("sign") == "saveBtn") {
-    saveWord();
+    var newContent = getTranslateContent();
+    saveWord(newContent);
+    popToast('add success');
   }else if(event.target.className == "historySpan") {
-    let word = event.target.innerHTML;
-    let parsedUrl = new URL(document.URL);
+    var word = event.target.innerHTML;
+    var parsedUrl = new URL(document.URL);
     parsedUrl.searchParams.set("text", word);
     window.location.href = parsedUrl.toString();
-  }else if()
-    }
+  }else if (event.target.getAttribute("sign") == "saveDicBtnDiv") {
+    var newContent = getSpecialWordInfo();
+    saveWord(newContent);
+    popToast('add success');
+  }
+}
 
+
+function popToast(msg) {
+  const options = {
+    settings: {
+      duration: 1000,
+    },
+    style: {
+      main: {
+        background: "#1a73e8",
+        color: "white",
+      },
+    },
+  };
+  toast.toast(msg, options);
+}
 
 function getTranslateContent() {
-  let content = document.getElementsByClassName('D5aOJc vJwDU')[0].innerHTML;
+  var content = document.getElementsByClassName('D5aOJc vJwDU')[0].innerHTML;
   return content;
 }
 
 addSaveBtn();
 observeDomChange();
 initHistoryDiv();
+
+const textChangeCallback = function(mutationsList, observer) {
+
+};
+var doc = document.getElementsByClassName('er8xn')[0];
+const config = { childList: true, subtree: true };
+const observer = new MutationObserver(textChangeCallback);
+observer.observe(doc, config);
