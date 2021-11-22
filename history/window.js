f9b41188611742a50aacb8c1797bc545cbc3dc0d
wildimagine <wildimagine@qq.com>
2021-11-22 13:02:12 +0800

    [fix]: add simple analysis to dictionary

--- history/window.js
+++ history/window.js
@@ -16,9 +16,67 @@ function toSpan(content) {
   return '<span class="historySpan">' + content + '</span>';
 }
 
+const noMeanings =   ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ">", "<", "?", "/", "[", "]", "{", "}", "|", "~", "\"", "'", ",", "."];
+function trimWord(iword) {
+  iword = iword.trim();
+  var c = iword.at(0);
+  var left = 0;
+  if(c && noMeanings.indexOf(c) >= 0) {
+    left += 1;
+  }
+  let right = iword.length ;
+   c = iword.at(-1);
+  if(c && noMeanings.indexOf(c) >= 0) {
+    right -= 1;
+  }
+  return iword.slice(left, right);
+}
+
+
+function analysis(dictionary) {
+  var lastFiveDayFreq = {}; // {key => count}
+  var fiveday = Object.keys(dictionary).slice(0, 4);
+  for(var key of fiveday) {
+    dictionary[key].forEach(function(content) {
+      // content is a sentences
+      var words = [];
+      if(content.indexOf(" ") >= 0 ) {
+        words = content.split(" ");
+      }else {
+        words = [content];
+      }
+      words.forEach(function(word) {
+        if(word) {
+          if(word.length >= 2){
+            word = trimWord(word);
+            if(lastFiveDayFreq[word]) {
+              lastFiveDayFreq[word] += 1;
+            }else {
+              lastFiveDayFreq[word] = 1;
+            }
+          }
+        }
+      });
+    });
+    var result = [];
+    for (var key in lastFiveDayFreq) {
+      if(lastFiveDayFreq[key] > 2) {
+        result.push(key);
+      }
+    }
+    return result;
+  }
+}
+
 function insertAllHistory(msg) {
-  for(var key in msg.data) {
-    var contents = msg.data[key];
+  var dictionary = msg.data;
+  var lastFre = analysis(dictionary);
+
+  var doc = toDiv(lastFre, "last Five day frequently checked");
+  parent.insertBefore(doc, fankui);
+
+  for(var key in dictionary) {
+    var contents = dictionary[key];
     var doc = toDiv(contents, key.substr(0, 10));
     parent.insertBefore(doc, fankui);
   }
